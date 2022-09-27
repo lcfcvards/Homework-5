93bc99afde96c26af6bf0bf01bee16e3961a50d9
lcfcvards <sytnik.dmitry@gmail.com>
2022-09-28 01:39:53 +0300

    Homework 5.1

---
+++ Homework 5.1.py
@@ -0,0 +1,9 @@
+a = int(input("Введите переменную а: "))
+b = int(input("Введите переменную b: "))
+c = int(input("Введите переменную c: "))
+
+if a > 10 and b > 10 and c > 10 and a % 3 == 0 and b % 3 == 0:
+    print("yes")
+else:
+    print("no")
+    
